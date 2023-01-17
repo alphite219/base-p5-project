3bf6d9203eca748065f51687544dbcc400e54d1f
alphite219 <alfiewright219@gmail.com>
2023-01-17 02:18:05 -0800

    Random B&W setup

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
     const CELL_SIZE = 40;
     createCanvas(800,800);
-    background(0);
+    noStroke();
+    let x = 0;
+    let y = 0;
+    while(y<height){
+        while(x<width){
+            fill(random(255));
+            rect(x,y,CELL_SIZE);
+            x += CELL_SIZE
+        }
+        y += CELL_SIZE
+        x = 0
+    }
+
 }
 function draw() {
 }
